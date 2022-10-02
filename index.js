7c21e4424540ccee0e56d6a5affd8cfe9d1038b6
Ihor Khomenko <igorhomenko97@gmail.com>
2022-10-02 22:58:10 +0300

    start hw

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+const contacts = require('./contacts')
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+    switch (action) {
+        case "list":
+            const allContacts = await contacts.listContacts();
+            console.log(allContacts);
+            break;
+    }
+}
+
+invokeAction({action: "list"})
