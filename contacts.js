9c3dc089e293a87ddc9e0e31920c649f1424fe28
Ihor Khomenko <igorhomenko97@gmail.com>
2022-10-03 18:57:05 +0300

    add functions

--- contacts.js
+++ contacts.js
@@ -3,10 +3,34 @@ const path = require("path");
 
 const contactsPath = path.join(`${__dirname}/db`, "contacts.json")
 
+const updateContacts = async(books) => await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+
 const listContacts = async()=> {
     const result = await fs.readFile(contactsPath);
     return JSON.parse(result);
 }
+
+const getContactById = async(id) => {
+    const allContacts = await listContacts()
+    const contactById = allContacts.find(el => el.id === id)
+    return contactById || null
+}
+
+const addContact = async (name, email, phone) => {
+    const allContacts = await listContacts()
+    const newContact = {
+        id: 
+        name,
+        email,
+        phone,
+    }
+    allContacts.push(newContact)
+    await updateContacts(allContacts)
+    return newContact
+
+}
 module.exports = {
     listContacts,
+    getContactById,
+    addContact
 }
--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
             const allContacts = await contacts.listContacts();
             console.log(allContacts);
             break;
+        case "get":
+            const contactById = await contacts.getContactById(id);
+            console.log(contactById)
+            break;
+        case "add":
+            
+    
     }
 }
 
-invokeAction({action: "list"})
+// invokeAction({action: "list"})
+invokeAction({action: "get", id: "4"})
